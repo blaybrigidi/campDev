4663b5bf94f0236171813b9b186185bfdffb5301
blaybrigidi <135924669+blaybrigidi@users.noreply.github.com>
2025-08-02 20:23:26 -0700

    Edited session controller to fix error

--- app/controllers/SessionsController.rb
+++ app/controllers/SessionsController.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def login
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-            render json: {message: "Login Successful"}, status: :ok
+            render json: {message: "Login Successful", user: user}, status: :ok
         else
             render json: {message: "Incorrect Email or Password"}, status: :unauthorized
         end
